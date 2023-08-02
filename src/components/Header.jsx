10c2c0c50d20171b139e6ddafec8cd4aaf8a4013
AntBezr <sosleepyanton@gmail.com>
2023-08-02 14:26:16 +0300

    routing

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <div>
+      header will be here 
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/CountryPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CountryPage(props) {
+  return (
+    <div>
+      Country page
+    </div>
+  );
+}
+
+export default CountryPage;
