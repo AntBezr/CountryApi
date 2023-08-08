1648f178049107257e4fe214010d254a0d1b0204
AntBezr <sosleepyanton@gmail.com>
2023-08-08 11:13:20 +0300

    styling card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import style from './Card.module.css'
 
-function Card(props) {
+function Card({flag,name,population, region, capital}) {
   return (
     <div className={style.card}>
       <div className={style.flagImg}>
-        img
+        {flag}
+      </div>
+      <div className={style.countryName}>
+         <h2>{name}</h2>
       </div>
       <div className={style.cardInfo}>
-        germ
+        <p>Population: {population}</p> 
+        <p>Region: {region}</p>
+        <p>Capital: {capital}</p>
+        
       </div>
     </div>
   );
